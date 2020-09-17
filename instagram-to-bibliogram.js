95baf494137aff78e2c9142fb2db3a7cc680d360
Edoardo Maria Elidoro <edoardo.elidoro@gmail.com>
2020-09-17 11:47:05 +0200

    Create instagram-to-bibliogram.js
    Added the userscript

---
+++ instagram-to-bibliogram.js
@@ -0,0 +1,27 @@
+// ==UserScript==
+// @name         Instagram to Bibliogram
+// @version      0.1
+// @description  redirect Instagram link to a Bibliogram instance
+// @author       edmael
+// @match        https://www.instagram.com/*
+// @include      https://www.instagram.com/*
+// @grant        none
+// ==/UserScript==
+(function() {
+    'use strict';
+    if (document.location.pathname.indexOf("/p/") == 0) {
+  window.stop(); // stop loading the window
+  location.hostname = 'bibliogram.pussthecat.org';
+        } else {
+            window.stop();
+            var newhostname = "bibliogram.pussthecat.org";
+            var newURL = location.protocol + "//"
+                + newhostname
+                + '/u'
+                + location.pathname
+                + location.search
+                + location.hash
+                ;
+            location.replace (newURL);
+        }
+})();
